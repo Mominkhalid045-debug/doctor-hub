f5acb90365d039247cda0fa788d1fce6aa1be8c9
Mominkhalid045-debug <muhammadmominkhalid045@gmail.com>
2026-06-13 12:44:59 -0700

    Fix Vercel SQLite tracing

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./prisma/dev.db'],
+  },
 };
 
 export default nextConfig;
